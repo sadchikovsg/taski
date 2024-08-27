3cd7e561cd702e8aba3f0969246a3ac17ee59fb3
ssg <ssg@nng.local>
2024-08-27 14:06:24 +0300

    📝 Change settings
    Change 'settings.py'.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,12 +1,20 @@
+import os
 from pathlib import Path
 
 import sentry_sdk
+from dotenv import load_dotenv
 from sentry_sdk.integrations.django import DjangoIntegration
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
+# SECRET_KEY = (
+#     'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^')
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default=(
+        'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'))
 
 DEBUG = False
 
